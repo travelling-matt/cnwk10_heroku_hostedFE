2c2ade2fd53280b7cca421f565c869892d79e160
Andrew Bolton <andrewbolton@Andrews-MacBook-Pro.local>
2021-12-20 11:08:17 +0000

    ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { PostCreator } from "./components/postCreator";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState();
+  const [post, setPost] = useState();
+  const [list, setList] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setList([...list, post]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{user ? `Welcome ${user}` : "Please sign in"}</h1>
+      <input onChange={(e) => setUser(e.target.value)} />
+      <PostCreator setPost={setPost} submitHandler={submitHandler} />
+      {list.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
